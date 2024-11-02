c582dc8efa781d8a0d8310a19f76dcc9164ee5b6
Dmitry Polubotko <dmitry.polubotko@gmail.com>
2024-11-02 19:17:54 +0300

    main.cpp: threadpool implementation added

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <random>
 #include <numeric>
+#include <queue>
 
 const size_t width = 29;
 const size_t height = 19;
@@ -229,10 +230,127 @@ size_t runGame(const Chromo& chromo)
 
 
 
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <condition_variable>
+#include <functional>
+
+//https://github.com/agruzdev/Yato/blob/master/modules/actors/yato/actors/private/thread_pool.h
+class thread_pool
+{
+    std::vector<std::thread> m_threads;
+    std::queue<std::function<void()>> m_tasks;
+
+    std::mutex m_mutex;
+    std::condition_variable m_cvar;
+
+    bool m_stop = false;
+
+public:
+    thread_pool(size_t threads_num) {
+        auto thread_function = [this] {
+            for(;;) {
+                std::function<void()> task;
+                {
+                    std::unique_lock<std::mutex> lock(m_mutex);
+                    m_cvar.wait(lock, [this] { return m_stop || !m_tasks.empty(); });
+                    if(m_stop && m_tasks.empty()) {
+                        break;
+                    }
+                    if(!m_tasks.empty()) {
+                        task = std::move(m_tasks.front());
+                        m_tasks.pop();
+                    }
+                }
+                // Execute
+                if(task) {
+                    try {
+                        task();
+                    }
+                    catch(std::exception & e) {
+                        (void)e;
+                        //TODO
+                    }
+                    catch(...) {
+                        //TODO
+                    }
+                }
+            }
+        };
+
+        for(size_t i = 0; i < threads_num; ++i) {
+            m_threads.emplace_back(thread_function);
+        }
+    }
+
+    ~thread_pool() {
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_stop = true;
+            m_cvar.notify_all();
+        }
+        for(auto & thread : m_threads) {
+            thread.join();
+        }
+    }
+
+    thread_pool(const thread_pool&) = delete;
+    thread_pool& operator=(const thread_pool&) = delete;
+
+    // Add task
+    template <typename FunTy_, typename ... Args_>
+    void enqueue(FunTy_ && function, Args_ && ... args) {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        if(!m_stop) {
+            m_tasks.emplace(std::bind(std::forward<FunTy_>(function), std::forward<Args_>(args)...));
+        }
+        else {
+            //TODO
+        }
+        m_cvar.notify_one();
+    }
+};
+
+#define PROCESS_THREADS
+
 size_t assignFitness(Population& pop, Chromo& best)
 {
     size_t bestReward = 0;
 
+#if defined(PROCESS_THREADS)
+    std::vector<size_t> rewards(populationSize);
+
+    {
+        size_t threadsAmount = 10;
+        thread_pool tPool(threadsAmount);
+
+        auto processLambda = [&](size_t start, size_t end){
+            for(size_t q = start; q < end; ++q)
+            {
+                size_t reward = runGame(pop[q].val);
+
+                rewards[q] = reward;
+
+                pop[q].fitness = reward;
+                if(reward == 0) pop[q].fitness = 1;
+            }
+        };
+
+        size_t batch = populationSize / threadsAmount;
+
+        for(size_t q = 0; q < threadsAmount; ++q)
+        {
+            size_t start = q * batch;
+            size_t end = (q + 1) * batch;
+            tPool.enqueue(processLambda, start, end);
+        }
+    }
+
+    auto maxEl = std::max_element(rewards.begin(), rewards.end());
+    bestReward = (*maxEl);
+    best = pop[maxEl - rewards.begin()].val;
+#else
     for(size_t q = 0; q < populationSize; ++q)
     {
         size_t reward = runGame(pop[q].val);
@@ -246,6 +364,7 @@ size_t assignFitness(Population& pop, Chromo& best)
         pop[q].fitness = reward;
         if(reward == 0) pop[q].fitness = 1;
     }
+#endif
 
     return bestReward;
 }
