37007fd3dd648dcd2f0aa4991853edc7ff31fcce
Dmitry Polubotko <dmitry.polubotko@gmail.com>
2024-11-10 13:35:26 +0300

    python: resnet-based dnn

--- python/predict_reward.py
+++ python/predict_reward.py
@@ -11,11 +11,15 @@ import os.path
 
 input_size = 551
 hidden_size = 100
-hidden_size2 = 10
+hidden_size2 = 50
 #~ hidden_size3 = 50
 num_classes = 1
+
 batch_size = 50
-learning_rate = 0.1
+
+learning_rate = 0.001
+#~ learning_rate = 0.1
+#~ learning_rate = 10.0
 
 class NeuralNet(nn.Module):
     def __init__(self):
@@ -23,60 +27,108 @@ class NeuralNet(nn.Module):
 
         torch.manual_seed(0)
 
-        #~ self.conv1 = nn.Conv2d(1, 16, 3, 2, 1)
+        self.conv1 = nn.Conv2d(1, 64, 3, 1, 1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 128, 3, 1, 1)
+        self.bn2 = nn.BatchNorm2d(128)
         #~ self.avgpool1 = nn.AvgPool2d(2, 2)
-        #~ self.conv2 = nn.Conv2d(16, 32, 3, 2, 1)
+        self.conv3 = nn.Conv2d(128, 256, 3, 2, 1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(256, 512, 3, 2, 1)
+        self.bn4 = nn.BatchNorm2d(512)
         
-        #~ self.tanh = nn.Tanh()
+        self.conv5 = nn.Conv2d(128, 256, 1, 2, 0)
+        self.conv6 = nn.Conv2d(256, 512, 1, 2, 0)
         
-        #~ self.fc1 = nn.Linear(32 * 3 * 4, hidden_size)
-        #~ self.fc2 = nn.Linear(hidden_size, hidden_size2)
-        #~ self.fc3 = nn.Linear(hidden_size2, num_classes)
-
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.act = nn.Sigmoid()
-        #~ self.bn = nn.BatchNorm1d(hidden_size)
+        self.act = nn.ReLU()
+        
+        self.fc1 = nn.Linear(512 * 5 * 8, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size2)
-        self.act2 = nn.Sigmoid()
-        #~ self.bn2 = nn.BatchNorm1d(hidden_size2)
         self.fc3 = nn.Linear(hidden_size2, num_classes)
+
+        #~ self.fc1 = nn.Linear(input_size, hidden_size)
+        #~ self.act = nn.ReLU()
+        #~ self.bn = nn.BatchNorm1d(hidden_size)
+        #~ self.fc2 = nn.Linear(hidden_size, num_classes)
+        #~ self.act2 = nn.Sigmoid()
+        #~ self.bn2 = nn.BatchNorm1d(hidden_size2)
+        #~ self.fc3 = nn.Linear(hidden_size2, num_classes)
         #~ self.act3 = nn.Sigmoid()
         #~ self.fc4 = nn.Linear(hidden_size3, num_classes)
 
 
-        #~ nn.init.xavier_uniform_(self.conv1.weight)
-        #~ nn.init.xavier_uniform_(self.conv2.weight)
+        nn.init.xavier_uniform_(self.conv1.weight)
+        nn.init.xavier_uniform_(self.conv2.weight)
+        nn.init.xavier_uniform_(self.conv3.weight)
+        nn.init.xavier_uniform_(self.conv4.weight)
+        nn.init.xavier_uniform_(self.conv5.weight)
+        nn.init.xavier_uniform_(self.conv6.weight)
         nn.init.xavier_uniform_(self.fc1.weight)
         nn.init.xavier_uniform_(self.fc2.weight)
         nn.init.xavier_uniform_(self.fc3.weight)
         #~ nn.init.xavier_uniform_(self.fc4.weight)
+        nn.init.constant_(self.bn1.weight, 1)
+        nn.init.constant_(self.bn2.weight, 1)
+        nn.init.constant_(self.bn3.weight, 1)
+        nn.init.constant_(self.bn4.weight, 1)
 
-        #~ nn.init.zeros_(self.conv1.bias)
-        #~ nn.init.zeros_(self.conv2.bias)
+        nn.init.zeros_(self.conv1.bias)
+        nn.init.zeros_(self.conv2.bias)
+        nn.init.zeros_(self.conv3.bias)
+        nn.init.zeros_(self.conv4.bias)
+        nn.init.zeros_(self.conv5.bias)
+        nn.init.zeros_(self.conv6.bias)
         nn.init.zeros_(self.fc1.bias)
         nn.init.zeros_(self.fc2.bias)
         nn.init.zeros_(self.fc3.bias)
         #~ nn.init.zeros_(self.fc4.bias)
+        nn.init.constant_(self.bn1.bias, 0)
+        nn.init.constant_(self.bn2.bias, 0)
+        nn.init.constant_(self.bn3.bias, 0)
+        nn.init.constant_(self.bn4.bias, 0)
 
     def forward(self, x):
-        #~ out = self.conv1(x)
-        #~ out = self.tanh(out)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.act(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.act(out)
+        out = out + x
+        
+        #~ print(out.shape)
+        
+        res = out
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out = self.act(out)
+        out = self.conv4(out)
+        out = self.bn4(out)
+        out = self.act(out)
+        out = out + self.conv6(self.conv5(res))
+        
+        #~ print(out.shape)
+        
         #~ out = self.avgpool1(out)
         #~ out = self.conv2(out)
-        #~ out = out.reshape(-1, 3 * 4 * 32)
-        #~ out = self.fc1(out)
-        #~ out = self.tanh(out)
-        #~ out = self.fc2(out)
-        #~ out = self.tanh(out)
-        #~ out = self.fc3(out) 
-
-        out = self.fc1(x)
-        out = self.act(out)
-        #~ out = self.bn(out)
-        out = self.fc2(out)
-        out = self.act2(out)
         #~ out = self.bn2(out)
-        out = self.fc3(out)
+        #~ out = self.act(out)
+        out = out.reshape(-1, 5 * 8 * 512)
+        out = self.fc1(out)
+        out = self.act(out)
+        out = self.fc2(out)
+        out = self.act(out)
+        out = self.fc3(out) 
+
+
+
+        #~ out = self.fc1(x)
+        #~ out = self.act(out)
+        #~ out = self.bn(out)
+        #~ out = self.fc2(out)
+        #~ out = self.act2(out)
+        #~ out = self.bn2(out)
+        #~ out = self.fc3(out)
         #~ out = self.act3(out)
         #~ out = self.fc4(out)
         
@@ -104,12 +156,14 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=
 total_dataset_size = 0
 
 dataset_size = 10000
-batch_input = torch.zeros(dataset_size, 1, 1, 551)
-#~ batch_input = torch.zeros(dataset_size, 1, 19, 29)
-batch_label = torch.zeros(dataset_size, 1, 1, 1)
+#~ batch_input = torch.zeros(dataset_size, 1, 1, 551)
+batch_input = torch.zeros(dataset_size, 1, 19, 29)
+#~ batch_label = torch.zeros(dataset_size, 1, 1, 1)
+batch_label = torch.zeros(dataset_size, 1)
 
-epochs = 500
-train_batch_size = 2048
+epochs = 200
+train_batch_size = 512
+#~ train_batch_size = 10000
 
 def fitness_func(ga_instance, solution, solution_idx):
     solution = solution > 0.5
@@ -128,9 +182,10 @@ def fitness_func(ga_instance, solution, solution_idx):
     if((total_dataset_size + select_batch) >= dataset_size):
         select_batch = select_batch - ((total_dataset_size + select_batch) - dataset_size)
     
-    inputs = torch.from_numpy(solution.reshape(solution.shape[0], 1, 1, 551).astype(np.float32))
-    #~ inputs = torch.from_numpy(solution.reshape(solution.shape[0], 1, 19, 29).astype(np.float32))
-    labels = torch.from_numpy(res[:, 0].reshape(solution.shape[0], 1, 1, 1).astype(np.float32))
+    #~ inputs = torch.from_numpy(solution.reshape(solution.shape[0], 1, 1, 551).astype(np.float32))
+    inputs = torch.from_numpy(solution.reshape(solution.shape[0], 1, 19, 29).astype(np.float32))
+    #~ labels = torch.from_numpy(res[:, 0].reshape(solution.shape[0], 1, 1, 1).astype(np.float32))
+    labels = torch.from_numpy(res[:, 0].reshape(solution.shape[0], 1).astype(np.float32))
     #~ print(f"s {select_batch} sol {solution.shape[0]}")
     batch_input[total_dataset_size:(total_dataset_size + select_batch)] = inputs[0:select_batch]
     batch_label[total_dataset_size:(total_dataset_size + select_batch)] = labels[0:select_batch]
@@ -146,11 +201,13 @@ def fitness_func(ga_instance, solution, solution_idx):
         #~ inputs = Variable(torch.from_numpy(solution.reshape(solution.shape[0], 1, 19, 29).astype(np.float32)).cuda())
 
         dataset = torch.utils.data.TensorDataset(batch_input, batch_label)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size = train_batch_size, num_workers = 0)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size = train_batch_size, num_workers = 0, shuffle=True)
         
         total_dataset_size = 0
         
         for epoch in range(1, epochs + 1):
+        #~ epoch = 1
+        #~ while(1):
             # monitor training loss
             trainLoss = 0.0
             for images, targets in dataloader:
@@ -165,11 +222,19 @@ def fitness_func(ga_instance, solution, solution_idx):
                   
             trainLoss = trainLoss / len(dataloader)
             
+            if(trainLoss < 100000.0): 
+                break
+            
             inputs = batch_input[0:50]
             inputs = Variable(inputs.cuda()) 
             predicted_reward = predict(inputs, model).cpu()
             
-            print(f"Epoch: {epoch:3d} Loss: {trainLoss:10.1f}    pred = {predicted_reward[0].item():7.0f} {predicted_reward[1].item():7.0f}    res = {batch_label[0][0][0][0]:7.0f} {batch_label[1][0][0][0]:7.0f}")
+            #~ print(f"Epoch: {epoch:3d} Loss: {trainLoss:10.1f}    pred = {predicted_reward[0].item():7.0f} {predicted_reward[1].item():7.0f}    res = {batch_label[0][0][0][0]:7.0f} {batch_label[1][0][0][0]:7.0f}")
+            #~ print(f"Epoch: {epoch:3d} Loss: {trainLoss:10.1f}    pred = {predicted_reward[0].item():7.0f} {predicted_reward[1].item():7.0f}    res = {batch_label[0][0]:7.0f} {batch_label[1][0]:7.0f}")
+            print(f"Epoch: {epoch:3d} Loss: {trainLoss:10.1f}    pred = {predicted_reward[0].item():7.0f} {predicted_reward[1].item():7.0f}    res = {batch_label[0][0]:7.0f} {batch_label[1][0]:7.0f}")
+            #~ epoch = epoch + 1
+        
+        torch.save({'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict()}, "predict_reward.ckp")
         
         #~ for i in range(30):
             #~ model.train()
@@ -226,11 +291,7 @@ def on_generation(ga_instance):
     
     if ga_instance.generations_completed % 50 == 0:
         ga_instance.save("pygad")
-        #~ torch.save(model, "predict_reward.ckp")
-        torch.save({
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            }, "predict_reward.ckp")
+        #~ torch.save({'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict()}, "predict_reward.ckp")
     
     
 if(os.path.isfile("predict_reward.ckp")):
--- python/predict_reward_infer.py
+++ python/predict_reward_infer.py
@@ -5,7 +5,7 @@ import os.path
 
 input_size = 551
 hidden_size = 100
-hidden_size2 = 10
+hidden_size2 = 50
 #~ hidden_size3 = 50
 num_classes = 1
 
@@ -15,25 +15,72 @@ class NeuralNet(nn.Module):
 
         #~ torch.manual_seed(0)
 
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.act = nn.Sigmoid()
-        #~ self.bn = nn.BatchNorm1d(hidden_size, affine=False)
+
+        self.conv1 = nn.Conv2d(1, 64, 3, 1, 1)
+        self.bn1 = nn.BatchNorm2d(64, affine=False)
+        self.conv2 = nn.Conv2d(64, 128, 3, 1, 1)
+        self.bn2 = nn.BatchNorm2d(128, affine=False)
+        #~ self.avgpool1 = nn.AvgPool2d(2, 2)
+        self.conv3 = nn.Conv2d(128, 256, 3, 2, 1)
+        self.bn3 = nn.BatchNorm2d(256, affine=False)
+        self.conv4 = nn.Conv2d(256, 512, 3, 2, 1)
+        self.bn4 = nn.BatchNorm2d(512, affine=False)
+        
+        self.conv5 = nn.Conv2d(128, 256, 1, 2, 0)
+        self.conv6 = nn.Conv2d(256, 512, 1, 2, 0)
+        
+        self.act = nn.ReLU()
+        
+        self.fc1 = nn.Linear(512 * 5 * 8, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size2)
-        self.act2 = nn.Sigmoid()
-        #~ self.bn2 = nn.BatchNorm1d(hidden_size2, affine=False)
         self.fc3 = nn.Linear(hidden_size2, num_classes)
+
+        #~ self.fc1 = nn.Linear(input_size, hidden_size)
+        #~ self.act = nn.ReLU()
+        #~ self.bn = nn.BatchNorm1d(hidden_size, affine=False)
+        #~ self.fc2 = nn.Linear(hidden_size, num_classes)
+        #~ self.act2 = nn.Sigmoid()
+        #~ self.bn2 = nn.BatchNorm1d(hidden_size2, affine=False)
+        #~ self.fc3 = nn.Linear(hidden_size2, num_classes)
         #~ self.act3 = nn.Sigmoid()
         #~ self.fc4 = nn.Linear(hidden_size3, num_classes)
 
     def forward(self, x):
         with torch.no_grad():
-            out = self.fc1(x)
+            
+            out = self.conv1(x)
+            out = self.bn1(out)
             out = self.act(out)
-            #~ out = self.bn(out)
+            out = self.conv2(out)
+            out = self.bn2(out)
+            out = self.act(out)
+            out = out + x
+            
+            res = out
+            out = self.conv3(out)
+            out = self.bn3(out)
+            out = self.act(out)
+            out = self.conv4(out)
+            out = self.bn4(out)
+            out = self.act(out)
+            out = out + self.conv6(self.conv5(res))
+            
+            out = out.reshape(-1, 5 * 8 * 512)
+            out = self.fc1(out)
+            out = self.act(out)
             out = self.fc2(out)
-            out = self.act2(out)
+            out = self.act(out)
+            out = self.fc3(out) 
+            
+            
+            
+            #~ out = self.fc1(x)
+            #~ out = self.act(out)
+            #~ out = self.bn(out)
+            #~ out = self.fc2(out)
+            #~ out = self.act2(out)
             #~ out = self.bn2(out)
-            out = self.fc3(out)
+            #~ out = self.fc3(out)
             #~ out = self.act3(out)
             #~ out = self.fc4(out)
         
@@ -50,8 +97,14 @@ model = NeuralNet()
 
 if(os.path.isfile("predict_reward.ckp")):
     checkpoint = torch.load("predict_reward.ckp")
-    #~ checkpoint['model_state_dict'].pop('bn.weight', None)
-    #~ checkpoint['model_state_dict'].pop('bn.bias', None)
+    checkpoint['model_state_dict'].pop('bn1.weight', None)
+    checkpoint['model_state_dict'].pop('bn1.bias', None)
+    checkpoint['model_state_dict'].pop('bn2.weight', None)
+    checkpoint['model_state_dict'].pop('bn2.bias', None)
+    checkpoint['model_state_dict'].pop('bn3.weight', None)
+    checkpoint['model_state_dict'].pop('bn3.bias', None)
+    checkpoint['model_state_dict'].pop('bn4.weight', None)
+    checkpoint['model_state_dict'].pop('bn4.bias', None)
     model.load_state_dict(checkpoint['model_state_dict'])
     
 print(f"fc1 {model.fc1.weight}")
@@ -70,7 +123,8 @@ val = np.concatenate((np.zeros(shape=(1, val.shape[1])), val), axis=0)
 print(val)
 
 
-predicted_reward = predict(torch.from_numpy(val.astype(np.float32)), model)
+predicted_reward = predict(torch.from_numpy(val.reshape(val.shape[0], 1, 19, 29).astype(np.float32)), model)
+#~ predicted_reward = predict(torch.from_numpy(val.astype(np.float32)), model)
 
 #~ print(f"pred = {predicted_reward[0].item():.0f} {predicted_reward[1].item():.0f} {predicted_reward[2].item():.0f}")
 print(f"pred = {predicted_reward}")
